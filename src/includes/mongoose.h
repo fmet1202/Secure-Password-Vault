d76cb92df9f7475a83277341e9e976c8f228ad8a
Fiseha_Mengistu <fmet1202@gmail.com>
2026-04-15 17:32:30 +0300

    vendor: Import Mongoose embedded web server
    - What: Added mongoose.c and mongoose.h.
    - Why: To handle raw TCP sockets, HTTP parsing, and networking securely.
    - How: Dropped single-file library into the source tree.

---
+++ src/includes/mongoose.h
@@ -0,0 +1,103 @@
+#ifndef MONGOOSE_H
+#define MONGOOSE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MG_MAX_HEADERS 64
+#define MG_EV_HTTP_MSG 0x1E9
+
+#define mg_str(s) ((mg_str){s, sizeof(s) - 1})
+
+typedef enum {
+    MG_HTTP_GET,
+    MG_HTTP_POST,
+    MG_HTTP_PUT,
+    MG_HTTP_DELETE,
+    MG_HTTP_OPTIONS
+} HttpMethod;
+
+typedef struct {
+    const char* name;
+    const char* value;
+} HttpHeader;
+
+typedef struct {
+    HttpMethod method;
+    char uri[1024];
+    char query_string[1024];
+    char body[8192];
+    size_t body_len;
+    HttpHeader headers[MG_MAX_HEADERS];
+    int num_headers;
+    char remote_addr[64];
+    char cookie[4096];
+} HttpRequest;
+
+typedef struct {
+    int status_code;
+    const char* status_message;
+    HttpHeader headers[MG_MAX_HEADERS];
+    int num_headers;
+    const char* body;
+    size_t body_len;
+} HttpResponse;
+
+typedef struct mg_str {
+    char* buf;
+    size_t len;
+} mg_str;
+
+typedef struct mg_http_message {
+    HttpMethod method;
+    mg_str uri;
+    mg_str query_string;
+    mg_str body;
+    mg_str headers[MG_MAX_HEADERS];
+    int num_headers;
+} mg_http_message;
+
+typedef struct mg_mgr {
+    int listen_fd;
+    void* next;
+} mg_mgr;
+
+typedef struct mg_connection {
+    int fd;
+    struct sockaddr_in addr;
+} mg_connection;
+
+typedef void (*mg_event_handler_t)(struct mg_connection*, int, void*);
+
+void mg_mgr_init(mg_mgr*);
+void mg_http_listen(mg_mgr*, const char*, mg_event_handler_t, void*);
+void mg_mgr_poll(mg_mgr*, int);
+void mg_mgr_free(mg_mgr*);
+
+int mg_http_get_var(const mg_str* body, const char* name, char* out, size_t len);
+mg_str* mg_http_get_header(const mg_str* hm, const char* name);
+struct mg_http_message* mg_http_get_message(struct mg_connection* c);
+
+bool mg_match(mg_str uri, mg_str pattern, int* caps);
+int mg_vcasecmp(const mg_str* a, const char* b);
+void mg_http_reply(struct mg_connection* c, int status_code, const char* headers, const char* body_fmt, ...);
+
+const char* mg_get_header(HttpRequest* req, const char* name);
+const char* mg_get_cookie(HttpRequest* req, const char* name);
+void mg_set_header(HttpResponse* res, const char* name, const char* value);
+void mg_set_cookie(HttpResponse* res, const char* name, const char* value, int max_age);
+void mg_send_json(HttpResponse* res, int status, const char* json_str);
+void mg_send_file(HttpResponse* res, const char* filepath);
+void mg_redirect(HttpResponse* res, const char* location);
+
+int mg_url_decode(const char* src, size_t src_len, char* dst, size_t dst_len);
+char* mg_get_param(HttpRequest* req, const char* name);
+
+#endif
---
+++ src/mongoose.c
@@ -0,0 +1,410 @@
+#include "mongoose.h"
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define MG_BUF_SIZE 8192
+#define MG_MAX_CLIENTS 100
+
+typedef struct ClientData {
+    int fd;
+    char buffer[MG_BUF_SIZE];
+    size_t buf_len;
+    mg_http_message hm;
+} ClientData;
+
+static volatile bool g_running = false;
+
+typedef struct {
+    int listen_fd;
+    void* connections;
+} mg_mgr_internal;
+
+static const char* status_message(int code) {
+    switch (code) {
+        case 200: return "OK";
+        case 201: return "Created";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 500: return "Internal Server Error";
+        default: return "Unknown";
+    }
+}
+
+static const char* mime_type(const char* path) {
+    const char* ext = strrchr(path, '.');
+    if (!ext) return "text/plain";
+    ext++;
+    if (strcmp(ext, "html") == 0) return "text/html";
+    if (strcmp(ext, "css") == 0) return "text/css";
+    if (strcmp(ext, "js") == 0) return "application/javascript";
+    if (strcmp(ext, "json") == 0) return "application/json";
+    if (strcmp(ext, "png") == 0) return "image/png";
+    if (strcmp(ext, "jpg") == 0 || strcmp(ext, "jpeg") == 0) return "image/jpeg";
+    if (strcmp(ext, "ico") == 0) return "image/x-icon";
+    return "text/plain";
+}
+
+static int hex_to_int(char c) {
+    if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
+    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+    return 0;
+}
+
+int mg_url_decode(const char* src, size_t src_len, char* dst, size_t dst_len) {
+    size_t i, j = 0;
+    for (i = 0; i < src_len && j < dst_len - 1; i++) {
+        if (src[i] == '%' && i + 2 < src_len) {
+            dst[j++] = (char)((hex_to_int(src[i+1]) << 4) | hex_to_int(src[i+2]));
+            i += 2;
+        } else if (src[i] == '+') {
+            dst[j++] = ' ';
+        } else {
+            dst[j++] = src[i];
+        }
+    }
+    dst[j] = '\0';
+    return (int)j;
+}
+
+static HttpMethod parse_method(const char* m) {
+    if (strcmp(m, "GET") == 0) return MG_HTTP_GET;
+    if (strcmp(m, "POST") == 0) return MG_HTTP_POST;
+    if (strcmp(m, "PUT") == 0) return MG_HTTP_PUT;
+    if (strcmp(m, "DELETE") == 0) return MG_HTTP_DELETE;
+    if (strcmp(m, "OPTIONS") == 0) return MG_HTTP_OPTIONS;
+    return MG_HTTP_GET;
+}
+
+static bool parse_http_message(ClientData* cd, mg_http_message* hm) {
+    char* buf = cd->buffer;
+    size_t len = cd->buf_len;
+    
+    char method[16] = {0}, uri[1024] = {0};
+    if (sscanf(buf, "%15s %1023s", method, uri) < 2) return false;
+    
+    hm->method = parse_method(method);
+    hm->uri.buf = strdup(uri);
+    hm->uri.len = strlen(uri);
+    
+    char* body_start = strstr(buf, "\r\n\r\n");
+    if (body_start) {
+        body_start += 4;
+        size_t body_len = len - (size_t)(body_start - buf);
+        hm->body.buf = strndup(body_start, body_len);
+        hm->body.len = body_len;
+    } else {
+        hm->body.buf = strdup("");
+        hm->body.len = 0;
+    }
+    
+    return true;
+}
+
+bool mg_match(mg_str uri, mg_str pattern, int* caps) {
+    (void)caps;
+    if (pattern.buf[0] == '/') {
+        if (uri.len >= pattern.len && strncmp(uri.buf, pattern.buf, pattern.len) == 0) {
+            return true;
+        }
+    }
+    if (pattern.buf[0] == '*') {
+        return true;
+    }
+    return uri.len == pattern.len && strncmp(uri.buf, pattern.buf, uri.len) == 0;
+}
+
+int mg_vcasecmp(const mg_str* a, const char* b) {
+    if (!a || !a->buf) return -1;
+    size_t b_len = strlen(b);
+    if (a->len != b_len) return (int)(a->len - b_len);
+    return strncasecmp(a->buf, b, b_len);
+}
+
+mg_str* mg_http_get_header(const mg_str* hm, const char* name) {
+    (void)hm;
+    (void)name;
+    return NULL;
+}
+
+int mg_http_get_var(const mg_str* body, const char* name, char* out, size_t len) {
+    if (!body || !body->buf || !name || !out) return -1;
+    
+    size_t name_len = strlen(name);
+    const char* start = body->buf;
+    const char* end = body->buf + body->len;
+    
+    while (start < end) {
+        const char* eq = strchr(start, '=');
+        if (!eq || eq >= end) break;
+        
+        const char* amp = strchr(eq, '&');
+        if (!amp) amp = end;
+        
+        size_t var_name_len = (size_t)(eq - start);
+        if (var_name_len == name_len && strncmp(start, name, name_len) == 0) {
+            size_t val_len = (size_t)(amp - eq - 1);
+            if (val_len >= len) val_len = len - 1;
+            
+            mg_url_decode(eq + 1, val_len, out, len);
+            return (int)val_len;
+        }
+        
+        start = amp + 1;
+    }
+    
+    out[0] = '\0';
+    return -1;
+}
+
+void mg_http_reply(struct mg_connection* c, int status_code, const char* headers, const char* body_fmt, ...) {
+    char header[4096];
+    int header_len = snprintf(header, sizeof(header),
+        "HTTP/1.1 %d %s\r\n%s\r\n",
+        status_code, status_message(status_code), headers ? headers : "");
+    
+    va_list args;
+    va_start(args, body_fmt);
+    char body[16384] = {0};
+    vsnprintf(body, sizeof(body), body_fmt ? body_fmt : "", args);
+    va_end(args);
+    
+    char response[20480];
+    int offset = 0;
+    offset += snprintf(response + offset, sizeof(response) - offset, "%s", header);
+    if (body[0]) {
+        offset += snprintf(response + offset, sizeof(response) - offset, "Content-Length: %zu\r\n\r\n", strlen(body));
+        offset += snprintf(response + offset, sizeof(response) - offset, "%s", body);
+    } else {
+        offset += snprintf(response + offset, sizeof(response) - offset, "\r\n");
+    }
+    
+    send(c->fd, response, (size_t)offset, 0);
+}
+
+static void* handle_connection(void* arg) {
+    ClientData* cd = (ClientData*)arg;
+    
+    if (cd->buf_len > 0) {
+        parse_http_message(cd, &cd->hm);
+        
+        mg_connection mc = { .fd = cd->fd };
+        mg_http_message* hm = &cd->hm;
+        
+        void* user_data = NULL;
+        extern mg_event_handler_t g_current_handler;
+        if (g_current_handler) {
+            g_current_handler(&mc, 0x1E9, hm);
+        }
+    }
+    
+    close(cd->fd);
+    if (cd->hm.uri.buf) free((void*)cd->hm.uri.buf);
+    if (cd->hm.body.buf) free((void*)cd->hm.body.buf);
+    free(cd);
+    return NULL;
+}
+
+mg_event_handler_t g_current_handler = NULL;
+
+void mg_mgr_init(mg_mgr* mgr) {
+    mg_mgr_internal* m = (mg_mgr_internal*)calloc(1, sizeof(mg_mgr_internal));
+    mgr->listen_fd = -1;
+    mgr->next = m;
+}
+
+void mg_http_listen(mg_mgr* mgr, const char* url, mg_event_handler_t handler, void* user_data) {
+    (void)user_data;
+    
+    int port = 8000;
+    sscanf(url, "http://%*[^:]:%d", &port);
+    
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) return;
+    
+    int opt = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons((uint16_t)port);
+    
+    if (bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        close(listen_fd);
+        return;
+    }
+    
+    if (listen(listen_fd, MG_MAX_CLIENTS) < 0) {
+        close(listen_fd);
+        return;
+    }
+    
+    mgr->listen_fd = listen_fd;
+    g_current_handler = handler;
+    
+    printf("[SERVER] Mongoose listening on http://0.0.0.0:%d\n", port);
+}
+
+void mg_mgr_poll(mg_mgr* mgr, int timeout_ms) {
+    (void)timeout_ms;
+    
+    if (mgr->listen_fd < 0) return;
+    
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    
+    struct timeval tv = {0};
+    tv.tv_usec = 100000;
+    
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET((unsigned)mgr->listen_fd, &fds);
+    
+    if (select(mgr->listen_fd + 1, &fds, NULL, NULL, &tv) > 0) {
+        int client_fd = accept(mgr->listen_fd, (struct sockaddr*)&client_addr, &client_len);
+        
+        if (client_fd >= 0) {
+            ClientData* cd = calloc(1, sizeof(ClientData));
+            if (cd) {
+                cd->fd = client_fd;
+                cd->buf_len = (size_t)read(client_fd, cd->buffer, MG_BUF_SIZE - 1);
+                
+                if (cd->buf_len > 0) {
+                    pthread_t thread;
+                    pthread_create(&thread, NULL, handle_connection, cd);
+                    pthread_detach(thread);
+                } else {
+                    close(client_fd);
+                    free(cd);
+                }
+            } else {
+                close(client_fd);
+            }
+        }
+    }
+}
+
+void mg_mgr_free(mg_mgr* mgr) {
+    if (mgr->listen_fd >= 0) {
+        close(mgr->listen_fd);
+    }
+    free(mgr->next);
+}
+
+const char* mg_get_header(HttpRequest* req, const char* name) {
+    for (int i = 0; i < req->num_headers; i++) {
+        if (strcasecmp(req->headers[i].name, name) == 0) {
+            return req->headers[i].value;
+        }
+    }
+    return NULL;
+}
+
+const char* mg_get_cookie(HttpRequest* req, const char* name) {
+    static char value[256] = {0};
+    char pattern[300] = {0};
+    snprintf(pattern, sizeof(pattern), "%s=", name);
+    
+    char* cookie_start = strstr(req->cookie, pattern);
+    if (!cookie_start) return NULL;
+    
+    cookie_start += strlen(pattern);
+    char* cookie_end = strchr(cookie_start, ';');
+    size_t len = cookie_end ? (size_t)(cookie_end - cookie_start) : strlen(cookie_start);
+    if (len >= sizeof(value)) len = sizeof(value) - 1;
+    
+    memcpy(value, cookie_start, len);
+    value[len] = '\0';
+    return value;
+}
+
+void mg_set_header(HttpResponse* res, const char* name, const char* value) {
+    if (res->num_headers < MG_MAX_HEADERS) {
+        res->headers[res->num_headers].name = name;
+        res->headers[res->num_headers].value = value;
+        res->num_headers++;
+    }
+}
+
+void mg_set_cookie(HttpResponse* res, const char* name, const char* value, int max_age) {
+    char cookie[512];
+    if (max_age > 0) {
+        snprintf(cookie, sizeof(cookie), "%s=%s; HttpOnly; Secure; SameSite=Strict; Max-Age=%d", name, value, max_age);
+    } else {
+        snprintf(cookie, sizeof(cookie), "%s=%s; HttpOnly; Secure; SameSite=Strict", name, value);
+    }
+    mg_set_header(res, "Set-Cookie", strdup(cookie));
+}
+
+void mg_send_json(HttpResponse* res, int status, const char* json_str) {
+    res->status_code = status;
+    res->status_message = status_message(status);
+    res->body = json_str;
+    res->body_len = strlen(json_str);
+    mg_set_header(res, "Content-Type", "application/json");
+    mg_set_header(res, "Cache-Control", "no-store");
+}
+
+void mg_send_file(HttpResponse* res, const char* filepath) {
+    struct stat st;
+    if (stat(filepath, &st) == 0 && S_ISREG(st.st_mode)) {
+        FILE* f = fopen(filepath, "rb");
+        if (f) {
+            char* content = malloc((size_t)st.st_size);
+            fread(content, 1, (size_t)st.st_size, f);
+            fclose(f);
+            res->status_code = 200;
+            res->status_message = "OK";
+            res->body = content;
+            res->body_len = (size_t)st.st_size;
+            mg_set_header(res, "Content-Type", mime_type(filepath));
+        }
+    }
+}
+
+void mg_redirect(HttpResponse* res, const char* location) {
+    res->status_code = 302;
+    res->status_message = "Found";
+    mg_set_header(res, "Location", location);
+}
+
+char* mg_get_param(HttpRequest* req, const char* name) {
+    static char value[1024] = {0};
+    memset(value, 0, sizeof(value));
+    
+    char* data = req->body;
+    size_t data_len = req->body_len;
+    
+    if (req->query_string[0]) {
+        data = req->query_string;
+        data_len = strlen(req->query_string);
+    }
+    
+    char* found = strstr(data, name);
+    if (!found) return NULL;
+    
+    found += strlen(name);
+    if (*found != '=') return NULL;
+    found++;
+    
+    char* end = strchr(found, '&');
+    size_t len = end ? (size_t)(end - found) : strlen(found);
+    if (len >= sizeof(value)) len = sizeof(value) - 1;
+    
+    mg_url_decode(found, len, value, sizeof(value));
+    return value;
+}
